080f08835f70cf29450d2e753fa205f46a6907e5
chenjiawei <chenjiawei@qq.com>
2020-07-04 15:54:54 +0800

    增加营业执照识别

--- src/idcardocr.php
+++ src/idcardocr.php
@@ -8,6 +8,7 @@ use TencentCloud\Common\Profile\ClientProfile;
 use TencentCloud\Common\Profile\HttpProfile;
 use TencentCloud\Ocr\V20181119\OcrClient;
 use TencentCloud\Ocr\V20181119\Models\IDCardOCRRequest;
+use TencentCloud\Ocr\V20181119\Models\BizLicenseOCRRequest;
 
 class idcardocr
 {
@@ -37,13 +38,33 @@ class idcardocr
      * @author: chen
      * @time: 2020/7/3 10:53
      */
-    public function cidcardocr($image_url = '', $cardSide = ''): string
+    public function cidcardocr(string $image_url = '', string $cardSide = ''): string
     {
         try {
             $req = new IDCardOCRRequest();
             $params = "{\"ImageUrl\":\"" . $image_url . "\",\"CardSide\":\"" . $cardSide . "\"}";
             $req->fromJsonString($params);
             $resp = $this->client->IDCardOCR($req);
+            return $resp->toJsonString();
+        } catch (RuntimeException $e) {
+            throw new RuntimeException($e->getMessage());
+        }
+    }
+
+    /**
+     * 营业执照识别
+     * @param string $image_url
+     * @return string
+     * @author: chen
+     * @time: 2020/7/3 10:53
+     */
+    public function bizlicenseocr(string $image_url = ''): string
+    {
+        try {
+            $req = new BizLicenseOCRRequest();
+            $params = "{\"ImageUrl\":\"" . $image_url . "\"}";
+            $req->fromJsonString($params);
+            $resp = $this->client->BizLicenseOCR($req);
             return $resp->toJsonString();
         } catch (RuntimeException $e) {
             throw new RuntimeException($e->getMessage());
